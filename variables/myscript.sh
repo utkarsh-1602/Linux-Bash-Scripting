14daa89a956cc520b519cf6ffb4052aac69cbf7e
utkarsh-1602 <usking619@gmail.com>
2024-02-28 04:50:21 +0530

    defining variables in bash

---
+++ variables/myscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+myname="utkarsh"
+myage="21" 
+
+echo "Hello my name is $myname"
+echo "I'm $myage years old"
+
+# this is a comment
+# don't use single quotes for defining the variables, it won't work
